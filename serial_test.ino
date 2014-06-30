c4701cae8c8cb26d616a6264118d3d1f731755bd
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-06-30 18:29:53 +0900

    serial_test.ino

---
+++ serial_test.ino
@@ -0,0 +1,39 @@
+void setup()
+{
+    Serial.begin(9600);
+}
+
+
+void recv_str(char *buf)
+{
+    int i = 0;
+    while (true) {
+        if (Serial.available()) {
+            buf[i] = Serial.read();
+            if (i > 7) break;
+            i++;
+        }
+    }
+    buf[9] = '\0';
+}
+
+
+void some_action(str)
+{
+    Serial.println(str);
+}
+
+
+void loop()
+{
+    if (Serial.available()) {
+        char str[10]
+        recv_str(str);
+        some_action(str);
+    }
+
+    else {
+        char str[10] = "s0n0s0n00"
+        some_action(str);
+    }
+}
