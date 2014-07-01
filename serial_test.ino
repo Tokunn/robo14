e539a5274da80f9eb78556624bf40633e25b795a
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-07-01 20:28:00 +0900

    refresh_update

--- serial_test.ino
+++ serial_test.ino
@@ -4,17 +4,17 @@ void setup()
 }
 
 
-void recv_str(char *buf, int count)
+void recv_str(char *buf)
 {
     int i = 0;
     while (true) {
         if (Serial.available()) {
             buf[i] = Serial.read();
-            if (i > (count - 2)) break;
+            if (i > 7) break;
             i++;
         }
     }
-    buf[(count - 1)] = '\0';
+    buf[9] = '\0';
 }
 
 
@@ -24,11 +24,11 @@ void some_action(char *str)
 }
 
 
-void main_()
+void loop()
 {
     if (Serial.available() > 0) {
         char str[10];
-        recv_str(str, 10);
+        recv_str(str);
         some_action(str);
     }
 
@@ -36,43 +36,4 @@ void main_()
         char str[10] = "s0n0s0n00";
         some_action(str);
     }
-}
-
-
-/*void loop()
-{
-    while (true) {
-        Serial.print("S");
-
-        if (Serial.available() > 0) {
-            char str_sync;
-
-            for (int j = 0; j < 4; j++) {
-                str_sync = Serial.read();
-                Serial.println(str_sync);
-
-                if (str_sync == 'O') {
-                    for (int i = 0; i < 10; i++) {
-                        Serial.print("A");
-                    }
-                    main_();
-                }
-            }
-        }
-        delay(10);
-    }
-}*/
-
-void loop()
-{
-    if (Serial.available() > 0) {
-        if (Serial.read() == 'S') {
-            for (int i = 0; i < 10; i++) {
-                Serial.print('A');
-            }
-            main_();
-        }
-    }
-
-    Serial.print(Serial.read());
 }
--- serial_test.py
+++ serial_test.py
@@ -10,7 +10,6 @@ def main(arduino):
     while True:
         for i in ['s0n0s0n0', 'f3r8b6l2', 'b8l3s0n0']:
             arduino.write(i)
-            #print(arduino.read())
         time.sleep(0.1)
 
 
@@ -25,17 +24,25 @@ def read_serial(arduino):
 if __name__ == '__main__':
     arduino = serial.Serial('/dev/ttyACM0', 9600)
 
-    while True:
-        chunck = read_serial(arduino)
-        print("Read SYN")
-        print(chunck)
-        if chunck == ['S']:
-            while True:
-                print("Write SYN/ACK\tRead ACK")
-                arduino.write('O')
-                chunck2 = read_serial(arduino)
-                if chunck2 == ['A']:
-                    main(arduino)
-                print(chunck2)
+    #while True:
+        #chunck = read_serial(arduino)
+        #print("Read SYN")
+        #print(chunck)
+        #if chunck == ['S']:
+            #while True:
+                #print("Write SYN/ACK\tRead ACK")
+                #arduino.write('O')
+                #chunck2 = read_serial(arduino)
+                #if chunck2 == ['A']:
+                    #main(arduino)
+                #print(chunck2)
 
-                time.sleep(0.05)
+                #time.sleep(0.05)
+
+    while True:
+        arduino.write('S')
+        print("Write S")
+        print(arduino.read())
+        if (arduino.read() == 'A'):
+            main()
+        time.sleep(0.05)
