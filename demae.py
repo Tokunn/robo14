d6b773c82ce5cd5326f6aee65ca8939c8d5a23db
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-07-04 17:51:26 +0900

    defaunlt2

--- demae.py
+++ demae.py
@@ -15,10 +15,63 @@ class Serial_write():
     def __init__(self):
         self.arduino = serial.Serial(arduino_port, 9600)
 
-    def make_serial(self):
-        self.serial_command = "$f0n0s0n00"
+
+    def serial_update(self, F710):
+        self.serial_command = ['$', 'f', '0', 'n', '0', 's', '0', 'n', '0', '0']
+
+        self.make_serial_direction(F710, "tire")
+        self.make_serial_direction(F710, "catarpillar")
+        self.make_serial_level2(F710)
+        self.make_serial_updown(F710)
+
+
+    def make_serial_direction(self, F710, ti_or_ca):
+        command_number = []
+        Axis_left_or_rigt = ''
+
+        if (ti_or_ca == "tire"):
+            command_number = [1, 3]
+            Axis_left_or_rigt = [F710.left_Axis_Y, F710.left_Axis_X]
+        elif (ti_or_ca == "catarpillar"):
+            command_number = [5, 7]
+            Axis_left_or_rigt = [F710.rigt_Axis_Y, F710.rigt_Axis_X]
+
+        if (Axis_left_or_rigt[0] > 0):
+            self.serial_command[command_number[0]] = 'f'
+        elif (Axis_left_or_rigt[0] < 0):
+            self.serial_command[command_number[0]] = 'b'
+        else:
+            self.serial_command[command_number[0]] = 's'
+
+        if (Axis_left_or_rigt[1] > 0):
+            self.serial_command[command_number[1]] = 'r'
+        elif (Axis_left_or_rigt[1] < 0):
+            self.serial_command[command_number[1]] = 'l'
+        else:
+            self.serial_command[command_number[1]] = 'n'
+
+
+
+
+    def make_serial_level(self, F710):
+        self.serial_command[2] = str(int(math.fabs(round(F710.left_Axis_Y * 10))))
+        self.serial_command[4] = str(int(math.fabs(round(F710.left_Axis_X * 10))))
+        self.serial_command[6] = str(int(math.fabs(round(F710.rigt_Axis_Y * 10))))
+        self.serial_command[8] = str(int(math.fabs(round(F710.rigt_Axis_X * 10))))
+
+    def make_serial_level2(self, F710):
+        pass
+
+    
+    def make_serial_updown(self, F710):
+        if (F710.Button_X):
+            self.serial_command[9] = '1'
+        else:
+            self.serial_command[9] = '0'
+
 
     def send_serial(self):
+        self.serial_command = ''.join(self.serial_command)
         self.arduino.write(self.serial_command)
         print("\t\t\t\t\t{0}".format(self.serial_command))
 
@@ -26,17 +79,27 @@ class Serial_write():
 def main():
 
     Arduino = Serial_write()
+    F710 = get_gamepad.LogicoolGamepad()
 
     while True:
-        Arduino.make_serial()
+        F710.update()
+
+        Arduino.serial_update(F710)
         Arduino.send_serial()
 
-        time.sleep(5)
+        if (F710.Button_Back + F710.Button_Strt == 2):
+            subprocess.call("sudo shutdown -h now".split())
+            return 0
+
+        time.sleep(0.3)
 
 
 if __name__ == '__main__':
     try:
         main()
+
+    except get_gamepad.GamepadError:
+        print("\nGamepadDevice not found !!")
 
     except KeyboardInterrupt:
         print("\nexit")
