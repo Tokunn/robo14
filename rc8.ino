ada8f4bb388f941998bb7ab2e90da3c8c2e8bffb
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-07-12 09:12:12 +0900

    rc9.ino-->rc8.ino

--- rc8.ino
+++ rc8.ino
@@ -1,114 +1,57 @@
-int fro_rea[] = {3, 4};
-int lef_rig[] = {6, 7};
-int speed[] = {9, 10, 11};
-
-
-boolean fb_dec[3][2] = {{LOW, LOW}, {HIGH, LOW}, {LOW, HIGH}};
-boolean lr_dec[3][2] = {{LOW, LOW}, {HIGH, LOW}, {LOW, HIGH}};
-boolean speed_dec[5][3] = {{LOW , LOW , LOW },
-                         {HIGH, LOW , HIGH},
-                         {HIGH, LOW , LOW },
-                         {HIGH, HIGH, HIGH},
-                         {HIGH, HIGH, LOW }};
-
-
 void setup()
 {
-  for (int i = 0; i < 2; i++){
-    pinMode(fro_rea[i], OUTPUT);
-  }
-  for (int i = 0; i < 2; i++){
-    pinMode(lef_rig[i], OUTPUT);
-  }
-  for (int i = 0; i < 3; i++){
-    pinMode(speed[i], OUTPUT);
-  }
-  Serial.begin(9600);
+    Serial.begin(9600);
+    pinMode(8, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    digitalWrite(8, HIGH);
+    digitalWrite(6, HIGH);
 }
 
 
 void recv_str(char *buf)
 {
-  int i = 0;
-  char c;
-  while (true) {
-    if (Serial.available()) {
-      c = Serial.read();
-      buf[i] = c;
-      if (i == 2) break;
-      i++;
+    if (Serial.read() == '$') {
+        int i = 0;
+        for(int i = 0; i < 4; i++) {
+            buf[i] = Serial.read();
+        }
+        buf[11] = '\0';
     }
-  }
-  buf[3] = '\0';
-}
-
-
-void make_list(char *buf, int *list)
-{
-  switch(buf[0]){
-    case 's':
-      list[0] = 0;
-      break;
-    case 'f':
-      list[0] = 1;
-      break;
-    case 'b':
-      list[0] = 2;
-      break;
-    default:
-      list[0] = 0;
-      break;
-  }
-  switch(buf[1]){
-    case '4':
-      list[2] = 0;
-      break;
-    case '5':
-      list[2] = 1;
-      break;
-    case '6':
-      list[2] = 2;
-      break;
-    case '7':
-      list[2] = 3;
-      break;
-    case '8':
-      list[2] = 4;
-      break;
-    case '9':
-      list[2] = 5;
-      break;
-    default:
-      list[2] = 0;
-      break;
-  }
-}
-
-
-void action(int *list)
-{
-  for (int i = 0; i < 2; i++){
-    digitalWrite(fro_rea[i], fb_dec[list[0]][i]);
-  }
-  for (int i = 0; i < 3; i++){
-    digitalWrite(speed[i], speed_dec[list[1]][i]);
-  }
 }
 
 
 void loop()
 {
-  if (Serial.available()){
-    char str[4];
-    recv_str(str);
+    digitalWrite(10, LOW);
+    char str[12] = "s0s0";
 
-    int list[3];
-    make_list(str, list);
+    if (Serial.available()) {
+        recv_str(str);
+    }
 
-    action(list);
-  }
-  else{
-    int list[3] = {0, 0, 0};
-    action(list);
-  }
+    if (str[0] == 'f') {
+        digitalWrite(8, HIGH);
+        digitalWrite(6, LOW);
+    }
+    /*else if (str[0] == 'b') {
+        digitalWrite(8, LOW);
+        digitalWrite(6, HIGH);
+    }
+    else if (str[0] == 's') {
+        digitalWrite(8, LOW);
+        digitalWrite(6, LOW);
+    }
+    else if (str[0] == '0') {
+        digitalWrite(8, HIGH);
+        digitalWrite(6, HIGH);
+    }*/ 
+    else {
+        digitalWrite(9, HIGH);
+    }
+
+    delay(50);
+    digitalWrite(10, HIGH);
+        
 }
