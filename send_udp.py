8967623c7fafaf1f6a35b1104f185c3b1261dab3
tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-07-14 23:22:50 +0900

    add send_udp.py

---
+++ send_udp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import socket
+import time
+from contextlib import closing
+
+def main1():
+    host = '127.0.0.1'
+    port = 4000
+    count = 0
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with closing(sock):
+        while True:
+            message = "Hello world : {0}".format(count).encode('utf-8')
+            print(message)
+            sock.sendto(message, (host, port))
+            count += 1
+            time.sleep(1)
+        return
+
+def main2():
+    host = '127.0.0.1'
+    port = 3794
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print ('Type massage...')
+    send_msg = raw_input()
+    serversock.sendto(send_msg, (host, port))
+
+if __name__ == '__main__':
+    main1()
+    #main2()
