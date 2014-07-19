02125a257673afaf5650259cf280dc3c4fa8a27c
tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-07-19 11:18:15 +0900

    update

--- ARDUINO/recv_udp.ino
+++ ARDUINO/recv_udp.ino
@@ -2,26 +2,14 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-    byte mac[] = { 0x80, 0xA2, 0xDA, 0x0F, 0x88, 0x32 };
-    IPAddress ip( 172, 16, 14, 200 );
+byte mac[] = { 0x80, 0xA2, 0xDA, 0x0F, 0x88, 0x32 };
+IPAddress ip( 172, 16, 14, 200 );
+unsigned int localPort = 4000;
+char packetBuffer[ UDP_TX_PACKET_MAX_SIZE ];
+EthernetUDP Udp;
 
-    unsigned int localPort = 4000;
-
-    char packetBuffer[ UDP_TX_PACKET_MAX_SIZE ];
-
-    EthernetUDP Udp;
-
-void UDP_setup() {
-    Ethernet.begin( mac, ip );
-    Udp.begin( localPort );
-}
-
-void get_command( int *command ) {
-    if( Udp.parsePacket() ) {
-        Udp.read( packetBuffer, UDP_TX_PACKET_MAX_SIZE );
-        Serial.println( packetBuffer );
-    }
-}
+void UDP_setup();
+void get_command( int *command );
 
 void setup() {
     UDP_setup();
@@ -33,4 +21,16 @@ void loop() {
 
     int command[] = { tire, 0, 0 };
     get_command( command );
+}
+
+void UDP_setup() {
+    Ethernet.begin( mac, ip );
+    Udp.begin( localPort );
+}
+
+void get_command( int *command ) {
+    if( Udp.parsePacket() ) {
+        Udp.read( packetBuffer, UDP_TX_PACKET_MAX_SIZE );
+        Serial.println( packetBuffer );
+    }
 }
