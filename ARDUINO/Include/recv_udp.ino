5e921e30d5a245427229cfeff81eccbbcac997b1
tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-07-19 10:11:26 +0900

    recv_serial.ino

--- ARDUINO/Include/recv_udp.ino
+++ ARDUINO/Include/recv_udp.ino
@@ -5,33 +5,20 @@
 
 class RecvUDP {
     public:
-        //void set_addr( byte *mac_addr, int *ip_addr );
-        //void set_port( int loclaport );
         void start();
         void get( char *command );
 
     private:
-        //byte *__mac;
-        //int *__ip;
-        int __port = 4000;
         char __packetBuffer[ UDP_TX_PACKET_MAX_SIZE ];
         int __packetSize;
         EthernetUDP __Udp;
+
+        int __port = 4000;
         IPAddress ip( 172, 16, 14, 200 );
         byte __mac[] = { 0x80, 0xA2, 0xDA, 0x0F, 0x88, 0x32 };
 };
 
-/*void RecvUDP::set_addr( byte *mac_addr, int *ip_addr ) {
-    __mac = mac_addr;
-    __ip = ip_addr;
-}
-
-void RecvUDP::set_port( int localport ) {
-    __port = localport;
-}*/
-
 void RecvUDP::start() {
-    //IPAddress ip( *__ip );
     Ethernet.begin( __mac, ip );
     Udp.begin( __port );
 }
