f243c6fa80a437e73fe9bffd6bbbfe08c99a074b
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-09-05 16:35:58 +0900

    autoRestart continue

--- RASPBERRYPI/autoRestart.py
+++ RASPBERRYPI/autoRestart.py
@@ -5,15 +5,33 @@
 ###     Designed On 2014.Sce.5 By Tokunn
 ###     Update On 2014.Sce.5 By Tokunn make
 
+import os
 import time
 import subprocess
 
+python_script = './main.py'
+cwd = './'
 
-if __name__ == '__main__':
+def main():
     while True:
         try:
-            subprocess.call( './main.py' )
+            p = subprocess.Popen(python_script, shell=True, cwd=cwd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+            (stdouterr, stdin) = (p.stdout, p.stdin)
+            print( '-' * 80 )
+            while True:
+                line = stdouterr.readline()
+                if not line:
+                    break
+                print line.rstrip()
+            print( "Return code: {0}".format( p.wait() ) )
+            print( '-' * 80 )
+            print('')
         except:
-            print("missing start python script")
-            pass
+            print("\nstop python_script")
         time.sleep(5)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nautoRestart Script exit")
